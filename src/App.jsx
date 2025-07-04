691ecb6dad2ad48542a880bc35fd251c9c25a1c5
sonpasavee975 <sunpasavee@gmail.com>
2025-07-05 00:37:08 +0700

    first 01-stopwatch commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState,useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2>01-Stopwatch</h2>
+    </>
+  )
+}
+
+export default App
