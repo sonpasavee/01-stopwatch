6772de5f7c730dbd99296db5d9098bf4d9b8e0aa
sonpasavee975 <sunpasavee@gmail.com>
2025-07-05 01:07:30 +0700

    01-stopwatch version1
    using useState and using useEffect and setInterval function

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,34 @@ import { useState,useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [time, setTime] = useState(0)
+  const [running , setRunning] = useState(false)
 
+  useEffect(()=>{
+    let interval
+    if(running) {
+      interval = setInterval(()=> {
+        setTime((prevTime) => prevTime + 10)
+      },10)
+    }else if(!running) {
+      clearInterval(interval)
+    }
+    return () => clearInterval(interval)
+  },[running])
   return (
     <>
-      <h2>01-Stopwatch</h2>
+      <div>
+        <h2 className='text-red-400'>01-Stopwatch</h2>
+          <div>
+            <span>{("0" + Math.floor(time/6000) % 60)}:</span>
+            <span>{("0" + Math.floor(time/1000) % 60)}:</span>
+            <span>{("0" + ((time/10) % 100)).slice(-2)}:</span>
+          </div>
+          <div>
+            {running ? (<button onClick={()=>{setRunning(false)}}>Stop</button>) : (<button onClick={()=>{setRunning(true)}}>Start</button>)}
+            <button onClick={()=>{setTime(0)}}>Clear</button>
+          </div>
+        </div>
     </>
   )
 }
